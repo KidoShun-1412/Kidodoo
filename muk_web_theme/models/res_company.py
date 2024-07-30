4f7e308fd5dbbf544f6c574b79b3cbdc150aec92
Loc Tran <xuanloc141210@gmail.com>
2024-07-30 16:40:53 +0700

    [ADD] web muk them modules

---
+++ muk_web_theme/models/res_company.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    
+    _inherit = 'res.company'
+    
+    #----------------------------------------------------------
+    # Fields
+    #----------------------------------------------------------
+    
+    favicon = fields.Binary(
+        string="Company Favicon", 
+        attachment=True
+    )
+    
+    background_image = fields.Binary(
+        string='Apps Menu Background Image',
+        attachment=True
+    )
+
+    font = fields.Selection(selection_add=[('Poppins', 'Poppins')])
